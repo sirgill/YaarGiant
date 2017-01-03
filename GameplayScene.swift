c4cd2a655dc03c4d9ce366a03c0325bbf6df3101
sirgill <sunnygill@Surinders-MacBook-Pro.local>
2017-01-03 09:34:46 -0500

    Gameplay Scene Created

---
+++ GameplayScene.swift
@@ -0,0 +1,16 @@
+//
+//  GameplayScene.swift
+//  YaarGiant
+//
+//  Created by Surinder Singh Gill on 1/3/17.
+//  Copyright © 2017 Surinder Singh Gill. All rights reserved.
+//
+
+import SpriteKit
+
+class GameplayScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+        print("It is loaded")
+    }
+}
